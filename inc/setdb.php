563615ee209cddd107afb55614e0942fe9fa9c13
Roger Wu <weayzehoa@gmail.com>
2025-02-21 21:43:46 +0800

    修改docker連線方式

--- inc/setdb.php
+++ inc/setdb.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
-    $link = mysqli_connect("localhost:3306","medianet","QWEqwe123!@#","codinglab");
+    // $link = mysqli_connect("localhost:3306","root","QWEqwe123!@#","codinglab"); 
+    $link = mysqli_connect("mysql57","root","QWEqwe123!@#","codinglab"); //for Docker connect
     mysqli_query($link,"set names utf8mb4") or die("資料庫GG了");
     date_default_timezone_set("Asia/Taipei");
     $time = date("Y-m-d H:i:s");
